67e547ef1e348b62e86e8d2ca44876de47689a1a
sergey968 <82058513+sergey968@users.noreply.github.com>
2021-08-20 16:52:00 +0400

    Выбор квартир

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    var currentFloor = 2;
+    var floorPath = $(".home-image path");
+    var counterUp = $(".counter-up");
+    var counterDown = $(".counter-down");
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
+
+    floorPath.on("mouseover", function() {
+        floorPath.removeClass("current-floor");
+        currentFloor = $(this).attr("data-floor");
+        $(".counter").text(currentFloor);
+    });
+
+    floorPath.on('click', toggleModal);
+
+    modalCloseButton.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
+
+    counterUp.on("click",function (){
+        if(currentFloor < 18) {
+            currentFloor++;
+            usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+            $(".counter").text(usCurrentFloor);
+            floorPath.removeClass("current-floor");
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");  
+        }
+    });
+
+    counterDown.on("click",function (){
+        if(currentFloor > 2) {
+            currentFloor--;
+            usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+            $(".counter").text(usCurrentFloor);
+            floorPath.removeClass("current-floor");
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");  
+        }
+    });
+
+    function toggleModal () {
+        modal.toggleClass("is-open");
+    }
+});
